2a502b3872afd1092f2d1d9af938a7a6e0d61a88
Keljrack <66731544+keljrackdevs@users.noreply.github.com>
2021-10-01 14:31:54 -0400

    Update Main.js

--- Renderer/Main.js
+++ Renderer/Main.js
@@ -1,1 +1,9 @@
-
+function DrawImage(name) {
+   window.onload = function() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    drawing = new Image();
+    drawing.src = "draw.png";
+    ctx.drawImage(drawing, 10, 10);
+  };
+}
